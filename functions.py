33bc7d53f8db2f009c3bf5d1cc321fbfe4799322
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-03-02 17:40:49 +0200

    refactor functions

--- functions.py
+++ functions.py
@@ -1,18 +1,22 @@
 def function1(puzzle):
     # return all the possible actions in given state (puzzle)
-    if puzzle[0] == 0:
+    if puzzle.index(0) == 0:
         return ['<', '^']
-    elif puzzle[2] == 0:
+    elif puzzle.index(0) == 1:
+        return ['>', '<', '^']
+    elif puzzle.index(0) == 2:
         return ['>', '^']
-    elif puzzle[3] == 0:
+    elif puzzle.index(0) == 3:
         return ['<', '^', 'v']
-    elif puzzle[4] == 0:
-        return ['<', '>', '^', 'v']
-    elif puzzle[5] == 0:
+    elif puzzle.index(0) == 4:
+        return ['>', '<', '^', 'v']
+    elif puzzle.index(0) == 5:
         return ['>', '^', 'v']
-    elif puzzle[6] == 0:
+    elif puzzle.index(0) == 6:
         return ['<', 'v']
-    elif puzzle[8] == 0:
+    elif puzzle.index(0) == 7:
+        return ['>', '<', 'v']
+    elif puzzle.index(0) == 8:
         return ['>', 'v']
 
 
@@ -22,11 +26,11 @@ def function2(selected_move, puzzle):
     # find index of zero in list
     index = puzzle.index(0)
 
-    if selected_move == '<':
+    if selected_move == '>':
         puzzle[index] = puzzle[index - 1]
         puzzle[index - 1] = 0
 
-    elif selected_move == '>':
+    elif selected_move == '<':
         puzzle[index] = puzzle[index + 1]
         puzzle[index + 1] = 0
 
@@ -38,6 +42,8 @@ def function2(selected_move, puzzle):
         puzzle[index] = puzzle[index + 3]
         puzzle[index + 3] = 0
 
+    return puzzle
+
 
 def function3(puzzle):
     # if puzzle is solved (in the correct order) return True, otherwise return False
@@ -45,4 +51,3 @@ def function3(puzzle):
         return True
     else:
         return False
-
